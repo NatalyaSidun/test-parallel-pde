4661395aec600b09a1e7585f3cfadec86c6bb2f4
NatalyaSidun <gesiona.ua@gmail.com>
2012-10-10 09:50:46 +0300

    Finished parabolic equations

--- windows/parabol/parabol.cpp
+++ windows/parabol/parabol.cpp
@@ -14,8 +14,10 @@
 using namespace System;
 using namespace std;
 
-double L, N, h, delta, T, K, koeff_teploprov, lambda, c, ro, gam;
-int i,j;
+double L, h, delta, T, koeff_teploprov, lambda, c, ro, gam;
+int i, j, N, K, Nh, Kh, Nstart, Nfinish, Kstart, Kfinish, num_threads;
+
+std::fstream resFile;
 
 inline double right_side(double x, double t)
 {
@@ -101,8 +103,8 @@ double parabol()
 double parallel_parabol(int num_threads)
 {
     double  **v = new  double *[N+1];
-      for (i = 0; i<=N; i++)
-            v[i]=new  double[K+1];
+      for (i = 0; i <= N; i++)
+            v[i] = new  double[K+1];
 
     double *x = new double[N+1];
     double *t = new double[K+1];
@@ -117,7 +119,7 @@ omp_set_num_threads(num_threads);
 #pragma omp parallel shared (h, delta, x, t, K, N, v, gam) private (i,j)
     {
    #pragma omp for 
-      for(int i = 0; i <= N-1; i++)
+      for(i= 0; i <= N-1; i++)
         {
           x[i]=(i-1) * h;
           for (j = 0;  j <= K-1; j++)
@@ -158,7 +160,6 @@ omp_set_num_threads(num_threads);
 }
 int main(array<System::String ^> ^args)
 {
-    delta = 0.001;
     L = 1;
     T = 100;
     N = 100;
@@ -167,11 +168,174 @@ int main(array<System::String ^> ^args)
     c = 460;
     ro = 7,7*10^3;
 
+    printf("Type length, ì: \n");
+	scanf_s ("%d",&L);
+
+    printf("Type time, ñ: \n");
+	scanf_s ("%d",&T);
+
+
+
+    printf("Type min coordinates mesh size: \n");
+	scanf_s ("%d",&Nstart);
+
+	printf("Type max coordinates mesh size: \n");
+	scanf_s ("%d",&Nfinish);
+
+
+	printf("Type min time mesh size: \n");
+	scanf_s ("%d",&Kstart);
+
+	printf("Type max time mesh size: ");
+	scanf_s ("%d",&Kfinish);
+
+	printf("Type coordinates increment step: \n");
+	scanf_s ("%d",&Nh);
+
+	printf("Type time increment step: \n");
+	scanf_s ("%d",&Kh);
+
+    resFile.open("result.txt", std::ios_base::out);
+
+    printf("Type lambda: \n");
+    printf("lambda = %5.5", lambda);
+   // scanf_s ("%5.5",&lambda);
+    resFile << "lambda =  ", 
+    resFile << lambda;
+    resFile << "\n";
+
+    printf("Type density: \n");
+    printf("density = %5.5", ro);
+    //scanf_s ("%5.5",&ro);
+    resFile << "density =  ", 
+    resFile << ro;
+    resFile << "\n";
+
+
+    printf("Type specific heat: \n");
+    printf("specific heat = %5.5", c);
+  //  scanf_s ("%10.10",&c);
+
+    resFile << "specific heat =  ", 
+    resFile << c;
+    resFile << "\n";
+
+
     koeff_teploprov = lambda/c*ro;
-    int num_threads = omp_get_num_procs();
-    double duration = parabol();
-    double parallel_duration = parallel_parabol(num_threads);
-    printf("duration = %10.10f \n", duration);
-    printf("parallel duration = %10.10f \n", parallel_duration);
-    getchar();
+    printf("Type thermal conductivity coefficient= %5.5", koeff_teploprov);
+
+    resFile << "thermal conductivity coefficient=  ", 
+    resFile << koeff_teploprov;
+    resFile << "\n";
+
+    
+    printf(" Time T = %d \n   ", T);
+
+    resFile << "Time T =  ", 
+	resFile << T;
+    resFile << "\n";
+
+    printf("Length L = %d \n   ", T);
+
+    resFile << "Length L =  ", 
+	resFile << L;
+    resFile << "\n";
+
+
+     printf("Min coordinates mesh size Nstart = %d \n   ", N);
+
+    resFile << "Min coordinates mesh size Nstart =  ", 
+	resFile << Nstart;
+    resFile << "\n";
+
+
+    printf("min time mesh size Kstart = %d \n   ", K);
+
+    resFile << "min time mesh size Kstart =  ", 
+	resFile << Kstart;
+    resFile << "\n";
+
+
+    printf("max coordinates mesh size Nstart = %d \n   ", N);
+
+    resFile << "max coordinates mesh size Nfinish =  ", 
+	resFile << Nfinish;
+    resFile << "\n";
+
+    printf("coordinates increment step Nh = %d \n   ", Nh);
+
+    resFile << "coordinates increment step Nh =  ", 
+	resFile << Nh;
+    resFile << "\n";
+
+
+    printf("time increment step Kh = %d \n   ", Kh);
+
+    resFile << "time increment step Kh =  ", 
+	resFile << Kh;
+    resFile << "\n";
+
+    printf("------Start Computing!------------- \n");
+    resFile << "----------Start Computing!------------- \n";
+    for (N = Nstart; N < Nfinish; N+=Nh)
+	{
+		for (K = Kstart; K <= Kfinish; K+=Kh)
+        {
+             printf(" K = %d \n   ", K);
+
+			  resFile << "K =  ", 
+			  resFile << K;
+		      resFile << "\n";
+
+              printf(" N = %d \n   ", N);
+
+			  resFile << "N =  ", 
+			  resFile << N;
+		      resFile << "\n";
+
+              double duration = parabol();
+
+               printf(" Linear duration = %10.10f \n   ", duration);
+
+			   resFile << "Linear Duration =   ", 
+			   resFile << duration;
+			   resFile << "\n";
+
+              for ( num_threads = 1; num_threads <= omp_get_num_procs(); num_threads++ )
+              {
+                  
+
+                 printf("Num threads = %d \n", num_threads);
+
+                 resFile << "Num threads =   ", 
+				 resFile << num_threads;
+				 resFile << "\n";
+
+                 double parallel_duration = parallel_parabol(num_threads);
+                 
+                 printf("Parallel duration = %10.10f \n", parallel_duration);
+
+                  resFile << "Parallel duration =   ", 
+				  resFile << parallel_duration;
+				  resFile << "\n";
+
+                  double acceleration = duration - parallel_duration;
+
+					 printf(" Acceleration = %10.10f \n   ", acceleration);
+
+					 resFile << "Acceleration =   ", 
+					 resFile << acceleration;
+					 resFile << "\n";
+
+
+                     printf("................................... \n");
+					 resFile << "...................... \n";
+              }
+
+               printf(".............../Finish iteration!/.................... \n");
+			   resFile << "........../Finish iteration!/............ \n";
+
+        }
+    }
+ resFile.close();
 }
